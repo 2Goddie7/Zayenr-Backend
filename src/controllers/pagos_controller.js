aac361174355d2666e06cff7becb989e50c72557
2Goddie7 <diego.mullo@epn.edu.ec>
2025-07-27 21:38:49 -0500

    crearcion pasarela de pagos

---
+++ src/controllers/pagos_controller.js
@@ -0,0 +1,35 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
+
+export const crearDonacion = async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Donación al Museo Gustavo Orcés',
+            },
+            unit_amount: req.body.monto * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:5173/donations/success',
+      cancel_url: 'http://localhost:5173/donations/cancel',
+    });
+
+    res.json({ id: session.id });
+  } catch (error) {
+    console.error('Error creando sesión de pago:', error);
+    res.status(500).json({ error: 'No se pudo crear la sesión de pago' });
+  }
+};
+
+export{
+    crearDonacion
+}
